880b09b16a70ef4e960dd617d3f5d65f27623316
Tyler Youngblood <tyleryoungblood@yahoo.com>
2015-01-09 11:28:39 -0500

    remove Compass @imports - will use autoprefixer instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp          = require('gulp');
 var browserSync   = require('browser-sync');
+var reload        = browserSync.reload;
+var sass          = require('gulp-sass');
 
 // Static Server
 gulp.task('browser-sync', function(){
@@ -8,4 +10,26 @@ gulp.task('browser-sync', function(){
       baseDir: "./"
     }
   });
+});
+
+// Sass task, will run when any SCSS file changes
+// will also automatically refresh broswer(s)
+gulp.task('sass', function(){
+  return gulp.src('theme/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('theme/css'))
+    // reload browser(s)
+    .pipe(reload({stream:true}));
+});
+
+// Default task(s) to be run by typing "gulp" in terminal
+gulp.task('default', function(){
+
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], {cwd: './'}, reload);
+  gulp.watch('theme/scss/**/*.scss', ['sass'], {cwd: './'}, reload);
 });
